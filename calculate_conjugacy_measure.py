1a814c8650a6bca3fbf5b8f1ebea743818ee05ee
Mikhail Yutman <myutman@gmail.com>
2021-10-05 20:05:07 +0300

    Faster version. Correct format. Right answer.

--- calculate_conjugacy_measure.py
+++ calculate_conjugacy_measure.py
@@ -1,6 +1,9 @@
+import sys
+
 from argparse import ArgumentParser
 
 import numpy as np
+import pandas as pd
 
 
 def parse_args():
@@ -16,54 +19,64 @@ def parse_args():
     return parser.parse_args()
 
 
+def verify_column(column):
+    return (column.dtype == np.float64 or column.dtype == np.int64) and (not np.isnan(column).any())
+
+
 def read_data(input_file):
-    records = []
-    with open(input_file) as inf:
-        for line in inf.readlines():
-            x, y = map(int, line.split(' '))
-            records.append((x, y))
+    records = pd.read_csv(input_file, delimiter=" ", names=["x", "y"])
+    if (not verify_column(records["x"])) or (not verify_column(records["y"])):
+        sys.stderr.write(f"Probably input format is incorrect.\nPlease make sure that input file contains sequence of rows containing space-separated pairs of integers.\n\n")
+        exit(0)
+
+    if len(records) < 9:
+        sys.stderr.write(f"Statistical test is only applicable to a sequence with at least 9 elements.\nInput file contains only {len(records)} lines which is less than needed.\n\n")
+        exit(0)
+
     return records
 
 
 def sort_records_by_x(records):
-    return sorted(records, key = lambda record: record[0])
+    return records.sort_values(by=["x"], ignore_index=True)
 
 
 def get_y_ranks(records):
-    ranks = [0 for _ in records]
-    ys_with_order = [(y, order) for order, (_, y) in enumerate(records)]
-    ys_with_order_sorted = sorted(ys_with_order, key = lambda y_with_order: y_with_order[0])
-    for rank, (_, order) in enumerate(ys_with_order_sorted):
-        ranks[order] = rank
-    return ranks
+    N = len(records)
+    records["rank"] = 0
+
+    sorting_order = records.sort_values(by=["y"]).index
+    records["rank"][sorting_order] = np.arange(N)[::-1]
+
+    subdfs = []
+    for _, subdf in records.groupby(by=["y"]):
+        subdf["rank"] = subdf["rank"].mean()
+        subdfs.append(subdf)
+
+    records = pd.concat(subdfs).sort_index()
+
+    return records["rank"]
 
 
-def calculate_r1_r2(ranks):
-    p = len(ranks) // 3
-    r1 = sum(ranks[:p])
-    r2 = sum(ranks[-p:])
-    return r1, r2
+def calculate_stats(ranks):
+    N = len(ranks)
+    p = N // 3
+    r1 = ranks[:p].sum()
+    r2 = ranks[-p:].sum()
+    rank_diff = round(r1 - r2)
+    std_dev = round((N + 0.5) * np.sqrt(p / 6))
+    conjugacy_measure = rank_diff / (p * (N - p))
+    return rank_diff, std_dev, conjugacy_measure
+
 
 if __name__ == '__main__':
     args = parse_args()
 
     records = read_data(args.input_file)
+
     records = sort_records_by_x(records)
     ranks = get_y_ranks(records)
 
-    for rank, (x, y) in zip(ranks, records):
-        print(x, y, rank)
-
-    r1, r2 = calculate_r1_r2(ranks)
-    print(r1, r2)
-
-    N = len(records)
-    p = N // 3
-    rank_diff = r1 - r2
-    std_dev = int((N + 0.5) * np.sqrt(p / 6))
-    conjugacy_measure = rank_diff / (p * (N - p))
-
-    print(rank_diff, std_dev, conjugacy_measure)
+    rank_diff, std_dev, conjugacy_measure = calculate_stats(ranks)
 
     with open(args.output_file, "w") as ouf:
-        ouf.write(f"{rank_diff} {std_dev} {conjugacy_measure}\n")
+        ouf.write(f"{rank_diff} {std_dev} {conjugacy_measure:.2}\n")
